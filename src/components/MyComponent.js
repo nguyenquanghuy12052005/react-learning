d1316458ce4490cf2c6bf503660f4f42cb28a629
nguyenquanghuy12052005 <daotran1252005@gmail.com>
2025-06-12 20:45:48 +0700

    components

---
+++ src/components/MyComponent.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+class MyComponents extends React.Component {
+    //  JSX
+    render(){
+        return (
+            <div>hellll-1 {Math.random()}
+
+            </div>
+        );
+   }
+}
+export default MyComponents;
