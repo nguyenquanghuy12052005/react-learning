d173ee1109dbe9be7498916640dc02229cf0bd04
nguyenquanghuy12052005 <daotran1252005@gmail.com>
2025-06-13 14:26:27 +0700

    event

--- src/components/MyComponent.js
+++ src/components/MyComponent.js
@@ -1,11 +1,35 @@
 import React from "react";
 
 class MyComponents extends React.Component {
+    state = {
+        name: 'huy nguyêmx ',
+        age: 20,
+        address: 'quang nam'
+    }
+handerClick = (event) => {
+// console.log("click me" + Math.random());
+console.log(this.state.name);
+console.log(this.state.age);
+this.setState({
+    name: 'quang huy ne',
+    age: Math.floor((Math.random() * 100) +1) //random 1->100
+})
+
+
+}
+
+onmoursoverClick(event) {
+// console.log("click me" + Math.random());
+console.log(event);
+}
+
     //  JSX
     render(){
         return (
-            <div>hellll-1 {Math.random()}
+            <div>hellll-1 {this.state.name} and {this.state.age}
+            <button onMouseOver={this.onmoursoverClick}>onMouseOver me</button>
 
+             <button onClick={this.handerClick}>Click me</button> 
             </div>
         );
    }
