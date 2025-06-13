5ee6c2261249408b8209b351576674459783c55c
nguyenquanghuy12052005 <daotran1252005@gmail.com>
2025-06-13 17:14:00 +0700

    show/hide thẻ div

--- src/components/DisplayInfo.js
+++ src/components/DisplayInfo.js
@@ -1,28 +1,41 @@
 import React from "react";
 
 class DisplayInfo extends React.Component {
+
+    state = {
+        ShowListUser: true
+    }
+
+      handleShowHide = () => {
+          this.setState( {
+            ShowListUser : !this.state.ShowListUser
+          })
+            }
+
+
     render() {
           const {listUser} = this.props;
-          console.log(listUser);
-          
+  
         return (
             //props
-          
             <div>
+                 <div>
+                     <span onClick={() => {this.handleShowHide()}}> {this.state.ShowListUser === true ? "show" : "hide"}</span>  
+                     </div>        
                 {listUser.map((user) => {
-                    return (
-                         <div key = {user.id}>
+                    return(
+               <div>    
+                    {this.state.ShowListUser && <div key = {user.id} className={user.age < 20 ? "red" : "green"}>
                             <div>name: {user.name}</div>
                             <div>age: {user.age}</div>
                             <hr></hr>
-                         </div>
-                         
+                         </div>  
+                         }
+                       
+                            </div>  
                     )
+
                 } )} 
-                {/* <div>Name: {name}</div>
-                <div>Age: {age}</div> */}
-                
-                
                 </div>
             
         )
