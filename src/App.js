a7abbc2ed1b9f500f148d7a09e232cd1f70ae52f
nguyenquanghuy12052005 <daotran1252005@gmail.com>
2025-06-19 15:26:53 +0700

    cài bootstrap và react-router-dom để điều hướng trang

--- src/App.js
+++ src/App.js
@@ -1,42 +1,22 @@
-import logo from './logo.svg';
+
 import './App.scss';
-import { useDispatch, useSelector } from 'react-redux';
-import { increaseCounter, decreaseCounter } from './redux/action/counterAction';
-import MyComponents from './components/MyComponent';
-import React from 'react';
-
-class App extends React.Component{
-  render(){
-    return (
-        <div>
-  đấ
-  <MyComponents></MyComponents>
-    </div>
-    )
-  }
-}
 
 
 
-// const App = () => {
-//   const count = useSelector(state => state.counter.count);
-//   const dispatch = useDispatch();
 
-//   return (
+
+
+const App = () => {
+ 
+
+  return (
     
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//         hihiihihih
-//         </p>
-//         <div>Count = {count}</div>
-//         <button onClick={() => dispatch(increaseCounter())}>Increase</button>
-//         <button onClick={() => dispatch(decreaseCounter())}>Decrease</button>
-//       </header>
-//     </div>
+    <div className="App-container">
+      huy nè
+      <button className='btn btn-primary'>test</button>
+    </div>
   
-//   );
-// }
+  );
+}
 
 export default App;
