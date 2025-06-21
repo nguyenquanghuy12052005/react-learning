3604440292a85418e60e90879c43d6426a42066b
nguyenquanghuy12052005 <daotran1252005@gmail.com>
2025-06-21 15:54:04 +0700

    lấy thông tin từ form

--- src/components/Admin/Content/ManageUser.js
+++ src/components/Admin/Content/ManageUser.js
@@ -1,5 +1,5 @@
 import ModelCreateUser from "./ModelCreateUser";
-
+import './ManageUser.scss'
 
 
 
@@ -12,8 +12,9 @@ const ManageUser  = (props) => {
            <div className="content">
             <button> add user  </button>
             <div className="table">
-                table <ModelCreateUser/>
+                table 
                 </div>
+                <ModelCreateUser/>
            </div>
         </div>
     )
--- src/components/Admin/Content/ModelCreateUser.js
+++ src/components/Admin/Content/ModelCreateUser.js
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import Button from 'react-bootstrap/Button';
 import Modal from 'react-bootstrap/Modal';
+import { FaFolderPlus } from "react-icons/fa6";
 
 const  ModelCreateUser = ()  => {
   const [show, setShow] = useState(false);
@@ -8,13 +9,32 @@ const  ModelCreateUser = ()  => {
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
 
+  
+   const [email, setEmail] = useState("");
+   const [password, setPassword] = useState("");
+   const [userName, setUserName] = useState("");
+   const [role, setRole] = useState("USER");
+   const [iamge, setImage] = useState("");
+   const [previewIamge, setPreviewImage] = useState("");
+
+
+   const handleUploadImage = (event) => {
+    if(event.target && event.target.files && event.target.files[0]){
+         setPreviewImage(URL.createObjectURL(event.target.files[0]))  ;
+         setImage(event.target.files[0]);
+    } else {
+        // setPreviewImage("")
+    }
+      
+   }
+
   return (
     <>
       <Button variant="primary" onClick={handleShow}>
         Launch demo modal
       </Button>
 
-      <Modal show={show} onHide={handleClose} size='xl' backdrop='static'>
+      <Modal show={show} onHide={handleClose} size='xl' backdrop='static' className='model-add-user'>
         <Modal.Header closeButton>
           <Modal.Title>Add new user</Modal.Title>
         </Modal.Header>
@@ -22,28 +42,36 @@ const  ModelCreateUser = ()  => {
         <form className="row g-3">
   <div className="col-md-6">
     <label  className="form-label">Email</label>
-    <input type="email" className="form-control" />
+    <input type="email" className="form-control" value={email} onChange={(event) => setEmail(event.target.value)}/>
   </div>
   <div className="col-md-6">
     <label className="form-label">Password</label>
-    <input type="password" className="form-control" />
+    <input type="password" className="form-control" value={password}  onChange={(event) => setPassword(event.target.value)}/>
   </div>
   
   <div className="col-md-6">
     <label  className="form-label">User Name</label>
-    <input type="text" className="form-control" />
+    <input type="text" className="form-control" value={userName}  onChange={(event) => setUserName(event.target.value)}/>
   </div>
   <div className="col-md-4">
     <label  className="form-label">Role</label>
-    <select  className="form-select">
-      <option selected value='USER'>USER</option>
+    <select  className="form-select" value={role} onChange={(event) => setRole(event.target.value)} >
+      <option  value='USER'>USER</option>
       <option value='ADMIN'>ADMIN</option>
     </select>
   </div>
  
  <div className='col-md-12'>
- <label className='form-label'>Image</label>
- <input type='file'/>
+ <label className='form-label lable-upload' htmlFor='fileUpload'> <FaFolderPlus/> Upload File Image</label>
+ <input type='file' hidden id='fileUpload'  onChange={(event) => handleUploadImage(event)}/> 
+ </div>
+ <div className='col-md-12 img-preview'>
+    
+    {previewIamge 
+    ? <img src={previewIamge}></img>
+    : <span>preview Index</span>
+     }
+   
  </div>
 
   
