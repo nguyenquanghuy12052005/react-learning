0f4667d8e3256b64cc5043168c8bff4ce75431d5
nguyenquanghuy12052005 <daotran1252005@gmail.com>
2025-10-29 22:57:07 +0700

    sửa ảnh voc

--- src/components/Vocab/VocabModal.js
+++ src/components/Vocab/VocabModal.js
@@ -1,9 +1,18 @@
 import React from 'react';
+import logo from '../../assets/logo5.png';
 
 const VocabModal = ({ word, onClose }) => {
   if (!word) return null;
 
   const { word: term, phonetic, meanings } = word;
+
+
+
+
+
+
+
+  
 
   return (
     <div className="modal fade show d-block" style={{ backgroundColor: 'rgba(0,0,0,0.5)' }} tabIndex="-1">
@@ -19,10 +28,28 @@ const VocabModal = ({ word, onClose }) => {
               <span className="badge bg-success fs-6 px-3 py-2">US {phonetic.us}</span>
               <span className="badge bg-info fs-6 px-3 py-2">UK {phonetic.uk}</span>
             </div>
-
+{/* 
             <div className="text-center mb-4">
               <div className="bg-light border border-dashed rounded-3 d-inline-block p-5">
-                <em className="text-muted">"{term}" picture</em>
+                <em className="text-muted">"{term}" {"https://www.bing.com/ck/a?!&&p=dcb39c989c5d0d2c22c0e0005412382fa4e1826507fb36e859e7425099ffe5abJmltdHM9MTc2MTY5NjAwMA&ptn=3&ver=2&hsh=4&fclid=2c6cd504-730b-6622-1cf6-c0e472a167c0&u=a1L2ltYWdlcy9zZWFyY2g_cT0lZTElYmElYTNuaCZpZD04RUYwQ0ExNzJEMjQzQzZEQzYwQ0ZGNThEODYzQjFGNkU3Njg1QkUyJkZPUk09SVFGUkJB"}</em>
+              </div>
+            </div> */}
+              <div className="text-center mb-4">
+              <div className="bg-light border rounded-3 p-3">
+          {word.image && (
+  <div className="text-center mb-3">
+    <img
+      src={word.image === "logo" ? logo : word.image}
+      alt={term}
+      className="img-fluid rounded shadow"
+      style={{ maxHeight: "200px", objectFit: "contain" }}
+    />
+  </div>
+)}
+
+                <div className="text-muted mt-2">
+                  <small>Ảnh minh họa cho "{term}"</small>
+                </div>
               </div>
             </div>
 
